55ff5d573d97d7de99f588c0eba376d8ffdfa3e1
tamasxfeher-glitch <tamas.x.feher@gmail.com>
2026-04-15 12:32:27 +0200

    Delete crypto-risk-pwa-netlify-ready directory

--- crypto-risk-pwa-netlify-ready/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' });
-}
-
-createRoot(document.getElementById('root')).render(<App />);
